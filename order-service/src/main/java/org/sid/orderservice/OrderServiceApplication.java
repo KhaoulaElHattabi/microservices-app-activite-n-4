50f6f1e0d9d4e4913bf5724edca7a4ba9e1792fa
KHAOULA <khaoulaelhattabi00@gmail.com>
2024-01-04 16:29:45 +0100

    add modifications

--- order-service/src/main/java/org/sid/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/sid/orderservice/OrderServiceApplication.java
@@ -1,6 +1,7 @@
 package org.sid.orderservice;
 
 import org.sid.orderservice.entities.Order;
+import org.sid.orderservice.entities.ProductItem;
 import org.sid.orderservice.enums.OrderStatus;
 import org.sid.orderservice.model.Customer;
 import org.sid.orderservice.model.Product;
@@ -33,20 +34,34 @@ public class OrderServiceApplication {
 			CustomerRestClientService customerRestClientService,
 			InventoryRestClientService inventoryRestClientService) {
 		return args -> {
-			List<Customer> customers =  customerRestClientService.allCustomers().getContent().stream().toList();
+			List<Customer> customers = customerRestClientService.allCustomers().getContent().stream().toList();
 			List<Product> products = inventoryRestClientService.allProducts().getContent().stream().toList();
 			Long customerId = 1L;
 			Random random = new Random();
-			Customer customer=customerRestClientService.customerById(customerId);
+			Customer customer = customerRestClientService.customerById(customerId);
 			for (int i = 0; i < 20; i++) {
-				Order order= Order.builder()
+				Order order = Order.builder()
 						.customerId(customers.get(random.nextInt(customers.size())).getId())
-						.status(Math.random()>0.5? OrderStatus.PENDING:OrderStatus.CREATED)
+						.status(Math.random() > 0.5 ? OrderStatus.PENDING : OrderStatus.CREATED)
 						.createdAt(new Date())
 						.build();
+				Order savedOrder = orderRepository.save(order);
+				for (int j = 0; j < products.size(); j++) {
+					if (Math.random() > 0.7) {
 
+						ProductItem productItem = ProductItem.builder()
+								.order(savedOrder)
+								.price(products.get(j).getPrice())
+								.productId(products.get(j).getId())
+								.quantity(random.nextInt(10))
+								.discount(Math.random())
+								.build();
+						productItemRepository.save(productItem);
+					}
+
+				}
 			}
 		};
+	}
 
-	};
 }
